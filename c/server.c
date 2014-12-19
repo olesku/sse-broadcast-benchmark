c34ab568f916c7c0ac41dfa684f2398adb1f554d
Ole Fredrik Skudsvik <ole.fredrik.skudsvik@vg.no>
2014-12-19 15:14:10 +0100

    Added support for POST /broadcast endpoint.

--- c/server.c
+++ c/server.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -25,13 +26,15 @@
 /* Defaults. */
 #define LISTEN_PORT 1942
 #define NUM_THREADS 4
-#define LOGLEVEL LOG_INFO
+#define LOGLEVEL LOG_DEBUG
 
 #define MAXEVENTS 1024
 
 #define HTTP_200 "HTTP/1.1 200 OK\r\n"
 #define HTTP_204 "HTTP/1.1 204 No Content\r\n"
 #define HTTP_404 "HTTP/1.1 404 Not Found\r\n"
+#define HTTP_400 "HTTP/1.1 400 Bad Request\r\n"
+
 
 #define RESPONSE_HEADER_STREAM      "Content-Type: text/event-stream\r\n" \
                                     "Cache-Control: no-cache\r\n" \
@@ -45,6 +48,7 @@
 
 #define RESPONSE_OPTIONS_CORS       "Access-Control-Allow-Origin: *\r\n" \
                                     "Connection: close\r\n\r\n"
+#define MAXBCAST_SIZE 8096
 
 typedef struct {
   pthread_t thread;
@@ -65,9 +69,15 @@ int efd;
 struct epoll_event *events;
 
 ssethread_t **threadpool;
-pthread_t t_main;
+pthread_t t_main, t_timefeed;
 
 pthread_mutex_t logmutex;
+pthread_mutex_t bcast_mutex;
+pthread_mutex_t do_ssebroadcast_mutex;
+
+pthread_cond_t do_ssebroadcast_cond;
+
+char bcast_buf[MAXBCAST_SIZE];
 
 void writelog(loglevel_t level, const char* fmt, ...) {
   va_list arglist;
@@ -133,42 +143,100 @@ int getnumclients() {
   return count;
 }
 
+int write_bcast(char *fmt, ...) {
+  char buf[MAXBCAST_SIZE];
+  int len;
+  va_list arglist;
+
+  va_start(arglist, fmt);
+  
+  vsnprintf(buf, MAXBCAST_SIZE, fmt, arglist);
+  len = strlen(buf);
+
+  pthread_mutex_lock(&do_ssebroadcast_mutex);
+  memcpy(&bcast_buf, buf, len);
+  bcast_buf[len] = '\0';
+  pthread_cond_broadcast(&do_ssebroadcast_cond); 
+  pthread_mutex_unlock(&do_ssebroadcast_mutex);
+ 
+  va_end(arglist);
+  
+  return len;
+}
+
+char* get_bcast() {
+  return bcast_buf;
+}
+
+/*
+ * Broadcasts data: timestamp every second.
+ */
+void *timestamp_feeder() {
+  struct timeval tv;
+
+  while (1) {
+    gettimeofday(&tv, NULL);
+    write_bcast("data: %ld\n\n", tv.tv_sec*1000LL + tv.tv_usec/1000);
+    sleep(1);
+  }
+}
+
 /*
  * SSE channel handler.
  */
 void *sse_thread(void* _this) {
  int t_maxevents, n, i;
  struct epoll_event *t_events, t_event;
- struct timeval tv;
  ssethread_t* this = (ssethread_t*)_this;
 
  t_maxevents = sysconf(_SC_OPEN_MAX) / NUM_THREADS;
  t_events = calloc(t_maxevents, sizeof(t_event));
  this->numclients = 0;
 
- while(1) {
-   n = epoll_wait(this->epollfd, t_events, t_maxevents, 0);
-   gettimeofday(&tv, NULL);
+  while(1) {
+    pthread_cond_wait(&do_ssebroadcast_cond, &do_ssebroadcast_mutex);
 
-   for (i = 0; i < n; i++) {
-     if ((t_events[i].events & EPOLLHUP) || (t_events[i].events & EPOLLRDHUP)) {
-       writelog(LOG_DEBUG, "sse_thread #%i: Client disconnected.", this->id);
-       close(t_events[i].data.fd);
-       this->numclients--;
-       continue;
-     }
+    n = epoll_wait(this->epollfd, t_events, t_maxevents, 0);
+    for (i = 0; i < n; i++) {
+      if ((t_events[i].events & EPOLLHUP) || (t_events[i].events & EPOLLRDHUP)) {
+        writelog(LOG_DEBUG, "sse_thread #%i: Client disconnected.", this->id);
+        close(t_events[i].data.fd);
+        this->numclients--;
+        continue;
+      }
 
-     if (t_events[i].events & EPOLLERR) {
-       writelog(LOG_ERROR, "sse_thread #%i: Socket error: %s.", this->id, strerror(errno));
-       continue;
-     }
+      if (t_events[i].events & EPOLLERR) {
+        writelog(LOG_ERROR, "sse_thread #%i: Socket error: %s.", this->id, strerror(errno));
+        continue;
+      }
 
-     writesock(t_events[i].data.fd, "data: %ld\n\n", tv.tv_sec*1000LL + tv.tv_usec/1000);
-   }
+       writesock(t_events[i].data.fd, "%s", get_bcast());
+    }
 
-   if (i > 0)  writelog(LOG_DEBUG, "Thread #%i: broadcasted to %i clients.", this->id, i);
-   sleep(1);
+    if (i > 0)  writelog(LOG_DEBUG, "Thread #%i: broadcasted to %i clients.", this->id, i);
   }
+}
+
+void post_broadcast_handler(int fd, char* buf) {
+  char *p, data[512];
+  int i;
+  p = strstr(buf, "data=");
+  
+  if (p == NULL) {
+    writesock(fd, "%sConnection: close\r\n\r\nInvalid broadcast request.", HTTP_400); 
+    return;
+  }
+
+  p+=5;
+
+  for (i=0; *p != '\0' && i < 512; p++, i++) {
+    data[i] = *p;
+  }
+
+  data[i] = '\0';
+
+  write_bcast("%s\n", data);
+  writesock(fd, "%sConnection: close\r\n\r\nBroadcasted '%s' to %i clients.\n", HTTP_200, data, getnumclients());
 }
 
 /*
@@ -187,7 +255,7 @@ void *main_thread() {
     for (i = 0; i < n; i++) {
       if ((events[i].events & EPOLLHUP) || (events[i].events & EPOLLRDHUP)) {
         close(events[i].data.fd);
-         continue;
+        continue;
       }
 
       if ((events[i].events & EPOLLERR) || (!(events[i].events & EPOLLIN))) {
@@ -204,6 +272,13 @@ void *main_thread() {
           close(events[i].data.fd);
           continue;
       }
+
+      if (strncmp(buf, "POST /broadcast", 15) == 0) {
+          post_broadcast_handler(events[i].data.fd, buf);
+          close(events[i].data.fd);
+          continue;
+      }
+
 
       if (get_uri(buf, len, uri, 64)) {
         writelog(LOG_DEBUG, "GET %s.", uri);
@@ -258,7 +333,6 @@ int main(int argc, char *argv[]) {
   /* Ignore SIGPIPE. */
   signal(SIGPIPE, SIG_IGN);
 
-
   /* Set up listening socket. */
   serversock = socket(AF_INET, SOCK_STREAM, 0);
   if (serversock == -1) {
@@ -288,8 +362,11 @@ int main(int argc, char *argv[]) {
   writelog(LOG_INFO, "Listening on port %i.", LISTEN_PORT);
 
   /* Initial client handler thread. */
-  t_main = pthread_create(&t_main, NULL, main_thread, NULL);
+  ret = pthread_create(&t_main, NULL, main_thread, NULL);
 
+  /* t_bcast */
+  ret = pthread_create(&t_timefeed, NULL, timestamp_feeder, NULL);
+  
   /* Set up SSE worker threads. */
   threadpool = (ssethread_t**)malloc(NUM_THREADS  * sizeof(ssethread_t*));
   for (i=0; i <= NUM_THREADS; threadpool[i++] = (ssethread_t*)malloc(sizeof(ssethread_t)));
